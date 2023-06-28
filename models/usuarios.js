8b087ed2b99a0dc65d98e12c1a7534b00c1d9aee
NelsonE <nelson.eustaquio@gmail.com>
2023-06-28 17:23:50 -0400

    primer modelo para usuario

---
+++ models/usuarios.js
@@ -0,0 +1,36 @@
+// Database
+const { Model, DataTypes, UUIDV4, literal } = require("sequelize");
+const { sequelize } = require("../config/database");
+
+class Usuario extends Model {}
+
+Usuario.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false
+  },
+  rol_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  login: {
+    type: DataTypes.STRING(10)
+  },
+  password: {
+    type: DataTypes.STRING(250)
+  },
+  zona: {
+    type: DataTypes.STRING(100)
+  },
+  status: {
+    type: DataTypes.STRING(1)
+  }
+}, {
+    underscored: true,
+    modelName: "usuario",
+  tableName: 'usuarios',
+  timestamps: false,
+  sequelize,
+});
+module.exports = Usuario;
