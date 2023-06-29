633f4f91aeeed821b98245e1bac81527ad52b575
NelsonE <nelson.eustaquio@gmail.com>
2023-06-28 21:25:24 -0400

    instalacion eslint

--- services/user_service.js
+++ services/user_service.js
@@ -1,2 +1,12 @@
 const UsuarioModel= require('..models/UsuarioModel');
+const { loginschema } = require('../middlewares/validations/user.validation');
 
+async function singIn(params) {
+    const response = {
+        message:"error",
+        status:400,
+        data:null,
+    };
+    const { login, password }= await loginschema.validateAsync(params);
+    console.log("===> ",login,password);
+}
